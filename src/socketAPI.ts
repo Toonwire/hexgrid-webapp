a2f05baedd7ddfc68beabecd6ab71959643ca1d2
Toonwire <toonwire@hotmail.com>
2023-09-03 02:14:11 +0200

    chore: expect optional player on game over

--- src/socketAPI.ts
+++ src/socketAPI.ts
@@ -1,5 +1,5 @@
 import { GameState } from '@toonwire/hexgrid-game-engine/game';
-import { PlayerCell } from '@toonwire/hexgrid-game-engine/player';
+import Player, { PlayerCell } from '@toonwire/hexgrid-game-engine/player';
 import { PlayerTransaction } from '@toonwire/hexgrid-game-engine/transaction';
 import io from 'socket.io-client';
 const ENDPOINT = 'http://127.0.0.1:4001';
@@ -27,8 +27,8 @@ function subscribeToGameState(callback: (gameState: GameState) => void) {
   socket.on('game_state', (newGameState: GameState) => callback(newGameState));
 }
 
-function subscribeToGameOver(callback: (winner: string | null) => void) {
-  socket.on('game_over', (_winner: string | null) => callback(_winner));
+function subscribeToGameOver(callback: (winner?: Player) => void) {
+  socket.on('game_over', (_winner?: Player) => callback(_winner));
 }
 
 function subscribeToTurn(callback: (myCells: PlayerCell[]) => void) {
